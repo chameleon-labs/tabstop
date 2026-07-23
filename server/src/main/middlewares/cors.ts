512bb9cc8349fede3fc7fbc134ce3e70107b3c2a
George Karan <georgekaran12@gmail.com>
2026-07-22 22:46:36 -0300

    Refine CORS middleware to restrict allowed methods and handle preflight requests

--- server/src/main/middlewares/cors.ts
+++ server/src/main/middlewares/cors.ts
@@ -1,8 +1,14 @@
 import type { NextFunction, Request, Response } from 'express'
 
-export const cors = (_req: Request, res: Response, next: NextFunction): void => {
+export const cors = (req: Request, res: Response, next: NextFunction): void => {
   res.set('access-control-allow-origin', '*')
-  res.set('access-control-allow-methods', '*')
+  res.set('access-control-allow-methods', 'GET,POST,PUT,PATCH,DELETE')
   res.set('access-control-allow-headers', '*')
+
+  if (req.method === 'OPTIONS') {
+    res.status(204).end()
+    return
+  }
+
   next()
 }
